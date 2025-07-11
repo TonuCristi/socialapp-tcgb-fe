123c6457890a3e1fdf1925574bd9fd645b282af2
Tonu Cristi <chrisdev2002@gmail.com>
2025-07-11 23:35:22 +0300

    setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,21 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import { ThemeProvider } from "styled-components";
+import { Provider } from "react-redux";
 
 import { GlobalStyle } from "./styles/GlobalStyle.ts";
 import { theme } from "./styles/theme.ts";
 import { router } from "./routes/router.tsx";
+import { store } from "./app/store.ts";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </Provider>
   </StrictMode>
 );
